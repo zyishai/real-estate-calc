a940eebfb80d1c76065b9a8edc2901199c10324f
zyishai <zehaviyishai@gmail.com>
2017-12-28 11:54:10 +0200

    Removed react native debugging tools
    * Instead installed microsoft debugging tools for react native in VS
    * Code.

--- App.js
+++ App.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 console.ignoredYellowBox = ['Remote debugger'];
 
 export default class App extends React.Component {
-  state = {
-    name: 'Yishai'
+  constructor(props) {
+    super(props)
   }
-
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{ this.state.name }</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
